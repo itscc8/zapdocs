2617e701a7947fac4cd81ad5bd7cd31e81c84c8f
itscc8 <gaming007yt@gmail.com>
2024-04-25 10:10:08 +0000

    Add new component and styles for Command module

---
+++ docs/test.md
@@ -0,0 +1,10 @@
+---
+id: test
+title: 'test'
+description: test
+sidebar_label: test
+---
+
+import Command from '@site/src/components/Command/Command'
+
+<Command name="/test" args={['test', 'test']}/>
--- src/components/Command/Command.js
+++ src/components/Command/Command.js
@@ -1,7 +1,36 @@
-export default function Command({ name }) {
+import { useState } from 'react'
+import styles from './Command.module.css'
+
+export default function Command({ name, args }) {
+  const [command, setCommand] = useState({ name, args })
+
+  function copy() {
+    navigator.clipboard.writeText(`${command.name} ${command.args.join(' ')}`)
+  }
+
   return (
-    <>
-      <h1>{name}</h1>
-    </>
+    <div className={styles.CommandContainer}>
+      <div className={styles.CommandMain}>
+        <b>{name}</b>
+        {args.map((arg, index) => (
+          <span
+            key={index}
+            className={styles.CommandArg}
+            contentEditable="true"
+            suppressContentEditableWarning="true"
+            onInput={(e) => {
+              const newArgs = [...command.args]
+              newArgs[index] = e.currentTarget.textContent
+              setCommand({ name: command.name, args: newArgs })
+            }}
+          >
+            {arg}
+          </span>
+        ))}
+      </div>
+      <div className={styles.CommandCopy} onClick={copy}>
+        Copy
+      </div>
+    </div>
   )
 }
